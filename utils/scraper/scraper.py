f629f13b687820a46467f08b901d5c01311c628f
Simranjyot Gill <simranjyot@Simranjyots-MBP.fios-router.home>
2017-07-07 15:29:11 -0400

    scraper working, but not looping through states

--- utils/scraper/scraper.py
+++ utils/scraper/scraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 LOGS_ENABLED = False
 DEBUG = False
@@ -21,41 +22,43 @@ def main():
 
     driver.get(url)
     assert webpage_title in driver.title
-    action_key_down_command = ActionChains(driver).key_down(Keys.COMMAND)
-    action_key_up_command = ActionChains(driver).key_up(Keys.COMMAND)
 
     state_select_element = driver.find_element_by_id("state")
     all_options = state_select_element.find_elements_by_tag_name("option")
     for option in all_options:
         if LOGS_ENABLED:
             print("Value for {0} is {1}".format(option.text, option.get_attribute("value")))
+            
+        state_name = option.text
+        file_path = 'raw-data/' + state_name
+
         option.click()
         next_page_button = driver.find_element_by_name("NextPage")
         next_page_button.click()
 
         # ----- Select agencies page -----
         # Selecting all agencies
-        agencies_select_element = driver.find_element_by_id("agencies")
-        all_agencies = agencies_select_element.find_elements_by_tag_name("option")
-        action_key_down_command.perform()
-        for agency in all_agencies:
-            if LOGS_ENABLED:
-                print("Value for {0} is {1}".format(agency.text, agency.get_attribute("value")))
-            agency.click()
-        action_key_up_command.perform()
+        agencies_select_element = Select(driver.find_element_by_id("agencies"))
+        for option in agencies_select_element.options:
+            agencies_select_element.select_by_visible_text(option.text)
 
         # Selecting all crime groups
-        crime_group_select_element = driver.find_element_by_id("groups")
-        all_crime_groups = crime_group_select_element.find_elements_by_tag_name("option")
-        action_key_down_command.perform()
-        for crime_group in all_crime_groups:
-            if LOGS_ENABLED:
-                print("Value for {0} is {1}".format(crime_group.text, crime_group.get_attribute("value")))
-            crime_group.click()
-        action_key_down_command.perform()
+        crime_group_select_element = Select(driver.find_element_by_id("groups"))
+        for option in crime_group_select_element.options:
+            crime_group_select_element.select_by_visible_text(option.text)
+
+        #Selecting year 2013
+        year_select = driver.find_element_by_xpath("//select[@id='year']/option[@value='2013']")
+        year_select.click()
 
         next_page_button = driver.find_element_by_name("NextPage")
         next_page_button.click()
+
+        # Reading data table
+        data_table = driver.find_element_by_xpath("//table[@title]").get_attribute('innerHTML')
+        with open(file_path,'w') as handle:
+            handle.write(data_table)
+
         input('wait')
 
 
